14f3d1d53f1ea94683ab902da9112124e5b34a67
ananyaaa26 <ananya.tuli@outlook.com>
2025-12-22 11:17:12 +0530

    Remove all DEMO_MODE code from codebase

--- src/app.js
+++ src/app.js
@@ -35,10 +35,11 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax'
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax"
     }
 }));
+
 
 // Set View Engine
 app.set('view engine', 'ejs');
