7395f30a152f89d30c966f31dbe70fe84b34cb2c
ananyaaa26 <ananya.tuli@outlook.com>
2025-12-22 23:02:27 +0530

    Fix Jest config to exclude Puppeteer integration tests

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   testEnvironment: 'node',
   coveragePathIgnorePatterns: ['/node_modules/'],
   testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  testPathIgnorePatterns: ['/node_modules/', '/tests/integration/'],
   verbose: true
 };
