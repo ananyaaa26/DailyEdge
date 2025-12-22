8baca8a7ea4aec04f6cf4e6077cda3e5e5a38522
ananyaaa26 <ananya.tuli@outlook.com>
2025-12-22 22:42:36 +0530

    Remove unnecessary setup files

--- create_admin.sql
+++
@@ -1,4 +0,0 @@
--- Create admin with password: admin123
-DELETE FROM admin WHERE email = 'test@dailyedge.com';
-INSERT INTO admin (username, email, password) 
-VALUES ('testadmin', 'test@dailyedge.com', '$2b$12$5SX1sUa6FvcYXtnKnkVkFeGO5XGq09blmz1fBGADDsZQgb9Sm457O');
